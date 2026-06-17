169ce54e9d4d53022b10b73fc8e178f1da47fc3f
Joshua Ravael <jravaellnew@gmail.com>
2026-06-17 20:00:22 +0700

    Refactor init_storage call in OnPolicyRunnerWithExtractor to improve argument clarity and use tensor initialization for observations and actions

--- Isaaclab_Parkour/scripts/rsl_rl/modules/on_policy_runner_with_extractor.py
+++ Isaaclab_Parkour/scripts/rsl_rl/modules/on_policy_runner_with_extractor.py
@@ -133,11 +133,13 @@ class OnPolicyRunnerWithExtractor(OnPolicyRunner):
             self.privileged_obs_normalizer = torch.nn.Identity().to(self.device)  # no normalization
         if self.depth_encoder_cfg is None:
             self.alg.init_storage(
-                self.env.num_envs,
-                self.num_steps_per_env,
-                {"policy": [num_obs], "critic": [num_privileged_obs]},
-                [self.env.num_actions],
-                [self.env.num_actions],
+                num_envs=self.env.num_envs,
+                num_transitions_per_env=self.num_steps_per_env,
+                obs={
+                    "policy": torch.zeros(num_obs, device=self.device),
+                    "critic": torch.zeros(num_privileged_obs, device=self.device),
+                },
+                actions=torch.zeros(self.env.num_actions, device=self.device),
             )
 
         self.disable_logs = self.is_distributed and self.gpu_global_rank != 0
